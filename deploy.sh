b7f72e38115d751e7055347f340eea2ec6c0b7f6
ilnur <thander66@gmail.com>
2023-10-05 17:02:42 +0700

    Add

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+POD_ID=`
+	runpodctl \
+	create pod \
+	--templateId '1iohg19etg' \
+	--imageName runpod/stable-diffusion:web-ui-10.2.1 \
+	--secureCloud \
+	--volumeSize 75 \
+	--containerDiskSize 30 \
+	--name ease \
+	--gpuType 'NVIDIA RTX A6000' \
+	--ports '8888/http,3001/http,22/tcp' \
+`;
+POD_ID=`echo "$POD_ID" | cut -d'"' -f 2`;
+/usr/bin/open -a "/Applications/Google Chrome.app" "https://$POD_ID-3001.proxy.runpod.net/"
+read -s -n 1 -p "Press any key to remove pod . . .";
+
+runpodctl remove pod $POD_ID;
+
---
+++ template.sh
@@ -0,0 +1,53 @@
+# insert to template (docker command)
+
+bash -c "echo '
+echo \"**** syncing venv to workspace, please wait. This could take a while on first startup! ****\"
+rsync --remove-source-files -rlptDu --ignore-existing /venv/ /workspace/venv/
+
+echo \"**** load models ****\"
+
+wget -q https://civitai.com/api/download/models/130072 -O /sd-models/realisticVisionV51.safetensors;
+
+echo \"**** syncing stable diffusion to workspace, please wait ****\"
+rsync --remove-source-files -rlptDu --ignore-existing /stable-diffusion-webui/ /workspace/stable-diffusion-webui/
+ln -s /sd-models/* /workspace/stable-diffusion-webui/models/Stable-diffusion/
+ln -s /cn-models/* /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet/models/
+
+echo \"**** load extensions and weights ****\"
+
+sed -i 's/--xformers//' /workspace/stable-diffusion-webui/webui-user.sh;
+git clone https://github.com/continue-revolution/sd-webui-animatediff /workspace/stable-diffusion-webui/extensions/sd-webui-animatediff;
+wget https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15_v2.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v14.ckpt;
+mv /workspace/mm_sd_v15_v2.ckpt /workspace/stable-diffusion-webui/extensions/sd-webui-animatediff/model/;
+mv /workspace/mm_sd_v14.ckpt /workspace/stable-diffusion-webui/extensions/sd-webui-animatediff/model/;
+cd /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet;
+git pull;
+
+if [[ $RUNPOD_STOP_AUTO ]]
+then
+    echo \"Skipping auto-start of webui\"
+else
+    echo \"Started webui through relauncher script\"
+    cd /workspace/stable-diffusion-webui
+    python relauncher.py &
+fi
+' > /pre_start.sh;
+/start.sh;"
+
+# wget -q https://civitai.com/api/download/models/130090 -O /sd-models/realisticVisionV51inpaint.safetensors;
+# cd /workspace/stable-diffusion-webui;
+
+# source /workspace/venv/bin/activate;
+# PYTHONPATH=/workspace/stable-diffusion-webui python extensions/sd-webui-segment-anything/install.py;
+# git clone https://github.com/continue-revolution/sd-webui-segment-anything  /workspace/stable-diffusion-webui/extensions/sd-webui-segment-anything;
+# mv /workspace/sam_vit_h_4b8939.pth /workspace/stable-diffusion-webui/extensions/sd-webui-segment-anything/models/sam/;
+# wget -q https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth;
+
+
+Negative: (deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck
+
+➜  100_ease mkdir -p resized && sips *.HEIC -Z 768 --cropToHeightWidth 512 512 --out resized/*.jpg
+
+ls -v | cat -n | while read n f; do mv -n "$f" "0$n.png"; done 
+
+runpodctl create pod --templateId '1iohfg19etx' --imageName runpod/stable-diffusion:web-ui-10.2.1 --volumeSize 75 --containerDiskSize 20 --secureCloud --name ease --gpuType 'NVIDIA RTX A4000' --ports '8888/http,3001/http,22/tcp'
